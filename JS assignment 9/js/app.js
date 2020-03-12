d01cb972585a20c7c721ff4bb00013bb830d7d50
Amreen <amreen.uk77@gmail.com>
2020-03-12 17:21:07 +0500

    JS_Assignment 9

---
+++ JS assignment 9/js/app.js
@@ -0,0 +1,99 @@
+//Assignment 9
+
+//Perform all these methods with example?
+
+
+//1) Math.round()
+
+var round1 = 77.80
+console.log("Round: " , Math.round(round1))
+
+var round2 = 77.40
+console.log("Round: " , Math.round(round2))
+
+var round3 = 77.5
+console.log("Round: " , Math.round(round3))
+
+//2) Math.ceil()
+
+var ceil1 = 79.80
+console.log("Ceil: " , Math.ceil(ceil1))
+
+var ceil2 = 79.40
+console.log("Ceil: " , Math.ceil(ceil2))
+
+var ceil3 = 79.5
+console.log("Ceil: " , Math.ceil(ceil3))
+
+//3) Math.floor()
+
+var floor1 = 55.80
+console.log("Floor : ", Math.floor(floor1))
+
+var floor2 = 55.40
+console.log("Floor : ", Math.floor(floor2))
+
+var floor3 = 55.5
+console.log("Floor : ", Math.floor(floor3))
+
+
+//4) Math.random()
+
+var random1 = Math.random()
+console.log("Random : " , random1)
+
+
+var random2 = Math.random() * 100
+console.log("Random between 1 to 100 : " , random2)
+
+
+var random3 = Math.random() * 50
+console.log("Random value with round method: " , Math.round(random3))
+
+//5) parseInt()
+
+var num1 = 20.33
+console.log("parseInt", parseInt(num1))
+
+var num2 = "33.40"
+console.log("parseInt", parseInt(num2))
+
+// //6) parseFloat()
+
+var num3 = 45.67
+console.log("parseFloat", parseFloat(num3))
+
+
+var num4 = "45.67"
+console.log("parseFloat", parseFloat(num4))
+
+// 7) toString()
+
+var num5 = 200
+console.log("convert to string: ", num5.toString())
+
+// 8) Number()
+
+var num6 = 22.37
+console.log("Number : ", Number(num6))
+
+
+var num7 = 22
+console.log("Number : ", Number(num7))
+
+// 9) toFixed()
+
+var num8 = 23.456789
+console.log("Number to be fixed: ",num8)
+console.log("fixed the value at point 3 : ", num8.toFixed(3))
+
+// 10) toUpperCase()
+
+var word = "pakistan zindabad"
+console.log("Turn into upper case: ", word.toUpperCase())
+
+// 11) toLowerCase()
+
+var word2 = "HELLO WORLD"
+console.log("turn into lower case: ",word2.toLowerCase())
+
