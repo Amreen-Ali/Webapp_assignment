597fb42bc5e41e903b2e6972fe9055a01f66fecd
Amreen <amreen.uk77@gmail.com>
2020-03-12 19:26:38 +0500

    JS_Assigment 11

---
+++ JS assignment 11/js/app.js
@@ -0,0 +1,87 @@
+//Assignment 11
+
+//Perform all these methods with example?
+
+//1) new Date();
+
+var date = new Date()
+console.log("Date method : ", date)
+
+//2) getDay();
+
+console.log("get day: ",date.getDay())
+
+//3) getMonth();
+
+console.log("get month : ", date.getMonth())
+
+//4) getDate();
+
+console.log("getdate : ", date.getDate())
+
+//5) getFullYear();
+
+console.log("getfullyear : ", date.getFullYear())
+
+//6) getHours();
+
+console.log("getHours : ", date.getHours())
+
+
+//7) getMinutes();
+
+console.log("getMinutes : ",date.getMinutes())
+
+//8) getSeconds();
+
+console.log("getSeconds : ", date.getSeconds())
+
+//9) getMiliSeconds();
+
+console.log("getMilliSeconds : ", date.getMilliseconds())
+
+// 10) getTime();
+
+console.log("getTime : ", date.getTime())
+
+// 11) setFullYear();
+
+var NewYear = new Date()
+NewYear.setFullYear("2035")
+console.log("SetFullYear : ", NewYear)
+
+// 12) setMonth();
+
+var NewMonth = new Date()
+NewMonth.setMonth(6)
+console.log("setMonth :  ", NewMonth)
+
+// 13) setDate();
+
+var NewDate = new Date()
+NewDate.setDate(21)
+console.log("setDate : ", NewDate)
+
+// 14) setHours();
+
+var NewHours = new Date()
+NewHours.setHours(21)
+console.log("SetHours : ", NewHours)
+
+// 15) setMinutes();
+
+var NewMinutes = new Date()
+NewMinutes.setMinutes(33)
+console.log("SetMinutes : ", NewMinutes)
+
+// 16) setSeconds();
+
+var NewSeconds = new Date()
+NewSeconds.setSeconds(56)
+console.log("SetSeconds : ", NewSeconds)
+
+// 17) setMiliSeconds()
+
+var NewMilliSeconds = new Date()
+NewMilliSeconds.setMilliseconds(21)
+console.log("SetMilliseconds : ", NewMilliSeconds)
