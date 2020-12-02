336f1453852471c342cff1856f475acd1d45da16
Amreen <amreen.uk77@gmail.com>
2020-12-02 19:43:20 +0500

    js assignment 16

---
+++ Javascript Assignments/JS assignment 16/js/app.js
@@ -0,0 +1,19 @@
+//ASSIGNMENT 16
+
+function saveNow(){
+    alert("You have clicked the right link.")
+}
+
+function saveButton(){
+    alert("You have clicked the right button.")
+}
+
+function GreetMe(){
+    var name = prompt("Enter Your Name?")
+    alert( "Hello " + name + "\n"+ "length of name is "+ name.length)
+    
+}
+
+function buy(){
+    alert("Thank you for Buying the mobile phone")
+}
